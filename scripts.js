cea6c61225e150eb228c65442827330dc8a34065
Emilly Karol <168681186+emillykarol@users.noreply.github.com>
2024-10-19 23:57:39 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,45 +5,57 @@ const todos = JSON.parse(localStorage.getItem('todos')) || []
 // JSON.stringify(localStorage.setItem('todos', ['correr', 'nadar']))
 // console.log(todos)
 // criar uma funçao para salvar tarefas
+
+
 const input = document.querySelector('.input')
 const container = document.querySelector('.container')
 const addButton = document.querySelector('.add')
 
-function salvarTarefas(){
+function salvarTarefas() {
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
+// funcao que criar a tarefa;sera reutilizada na addTarefa e na inicializacao da lista que esta no localStorage
+function createTarefa(nome) {
+    const itemBox = document.createElement('div');
+    itemBox.classList.add('item');
+
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.classList.add('check-box');
+
+    const inputTask = document.createElement('input');
+    inputTask.type = 'text';
+    inputTask.disabled = true;
+    inputTask.value = nome;
+    inputTask.classList.add('item-input');
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.innerText = 'EDITAR';
+    editBtn.addEventListener('click', () => editTarefa(inputTask, nome));
+
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove');
+    removeBtn.innerText = 'REMOVER';
+    removeBtn.addEventListener('click', () => removeTarefa(itemBox, nome));
+
+    container.append(itemBox);
+    itemBox.append(checkBox);
+    itemBox.append(inputTask);
+    itemBox.append(editBtn);
+    itemBox.append(removeBtn);
+}
+
+
 function addTarefa(nome) {
-    const itemBox = document.createElement('div')
-    itemBox.classList.add('item')
-
-    const inputTask = document.createElement('input')
-    inputTask.type = 'text'
-    inputTask.disabled = true
-    inputTask.value = nome
-    inputTask.classList.add('item-input')
-
-    const editBtn = document.createElement('button')
-    editBtn.classList.add('edit')
-    editBtn.innerText = 'EDITAR'
-    editBtn.addEventListener('click', () => editTarefa(inputTask, nome))
-
-
-    const removeBtn = document.createElement('button')
-    removeBtn.classList.add('remove')
-    removeBtn.innerText = 'REMOVER'
-    removeBtn.addEventListener('click', () => removeTarefa(itemBox, nome))
-
-    container.append(itemBox)
-    itemBox.append(inputTask)
-    itemBox.append(editBtn)
-    itemBox.append(removeBtn)
-
-    todos.push(nome)
-    
-
-    input.value = ''
-    input.focus()
+    if (nome.trim() !== '' && !todos.includes(nome)) {
+        todos.push(nome);
+        salvarTarefas();
+        createTarefa(nome);
+        input.value = '';
+        input.focus();
+    }
 }
 
 function editTarefa(input, tarefa) {
@@ -59,29 +71,21 @@ function editTarefa(input, tarefa) {
 function removeTarefa(itemBox, tarefa) {
     container.removeChild(itemBox)
     const index = todos.indexOf(tarefa)
-    todos.splice(index,1)
-    salvarTarefas()
+    if (index > -1) {
+        todos.splice(index, 1)
+        salvarTarefas() 
+    }
 }
-
 
 addButton.addEventListener('click', () => {
     addTarefa(input.value)
-    salvarTarefas()
 })
 
 input.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         addTarefa(input.value)
-        salvarTarefas()
     }
 })
 
-
-// for(const tarefa of todos) {
-//     addTarefa(tarefa)
-// }
-
-
-todos.forEach(task => {
-    addTarefa(task)
-});
+// inicializacao da lista que esta no localStorage
+todos.forEach(task => createTarefa(task));
